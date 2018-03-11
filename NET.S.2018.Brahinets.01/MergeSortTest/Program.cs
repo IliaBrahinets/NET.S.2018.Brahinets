3951a2c8a1aad4d7e5cf79a0c3daaaef54c9ae32
djbragas <windows888xxx@gmail.com>
2018-03-11 11:51:28 +0300

    Day1 homework

---
+++ NET.S.2018.Brahinets.01/MergeSortTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using IListExtensions;
+
+namespace MergeSortTest
+{
+    public class Program
+    {   
+        public static int ReverseCompare(int x, int y)
+        {
+            return y - x;
+        }
+
+        public static void RandFilling(List<int> array, int count, int maxValue)
+        {
+            Random randomGen = new Random(DateTime.Now.GetHashCode());
+
+            for (int i = 0; i < count; i++)
+            {
+                array.Add(randomGen.Next() % maxValue);
+            }
+        }
+
+        public static void PrintArray(List<int> array)
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(array[i].ToString() + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            List<int> array = new List<int>();
+
+            int maxValue = 1000;
+            int count = 20;
+
+            RandFilling(array, count, maxValue);
+            array.MergeSort();
+            Console.WriteLine("DefaultCompare:");
+            PrintArray(array);
+            array.Clear();
+
+            RandFilling(array, count, maxValue);
+            array.MergeSort(ReverseCompare);
+            Console.WriteLine("ReverseCompare:");
+            PrintArray(array);
+            array.Clear();
+
+            RandFilling(array, count, maxValue);
+            array.MergeSort(0, (count / 2) - 1, null);
+            Console.WriteLine("Sort the first half of array:");
+            PrintArray(array);
+            array.Clear();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NET.S.2018.Brahinets.01/MergeSortTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MergeSortTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MergeSortTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("572a4572-a24b-472b-a066-2b5c5a3f0d14")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
