7858d9f030ffebe79f6eed40357aca51586a7db7
djbragas <windows888xxx@gmail.com>
2018-04-05 17:54:44 +0300

    Day 11 Initial logging structure.

---
+++ NET.S.2018.Brahinets.11/Logging/LogLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    enum LogLevel
+    {
+        Trace = 0,
+        Debug = 20,
+        Info = 40,
+        Warn = 50,
+        Error = 100,
+        Fatal = 200
+    }
+}
