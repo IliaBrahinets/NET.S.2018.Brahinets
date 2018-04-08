0fb0e71b0f453cd8fe212150a489c7c0811c778d
djbragas <windows888xxx@gmail.com>
2018-04-08 18:02:51 +0300

    Day 06 Some comments was added.

--- NET.S.2018.Brahinets.06/JaggedArraySort/SortCriteriasAsStaticMethods.cs
+++ NET.S.2018.Brahinets.06/JaggedArraySort/SortCriteriasAsStaticMethods.cs
@@ -7,17 +7,43 @@ using System.Threading.Tasks;
 
 public class SortCriteriasAsStaticMethods
 {
-
+    /// <summary>
+    /// Represent comparing two one dimensional arrays.
+    /// As a key of comparing the sum of elements is used.  
+    /// null is less than non-null.
+    /// </summary>
+    /// <returns>x > y -> more than zero
+    ///          x < y -> less than zero
+    ///          x = y -> zero
+    /// </returns>
     public static int ByRowsSum(int[] x, int[] y)
     {
         return new ByRowsSumComparer().Compare(x,y);
     }
 
+    /// <summary>
+    /// Represent comparing two one dimensional arrays.
+    /// As a key of comparing the max elements of arrays is used.  
+    /// null is less than non-null.
+    /// </summary>
+    /// <returns>x > y -> more than zero
+    ///          x < y -> less than zero
+    ///          x = y -> zero
+    /// </returns>
     public static int ByRowsMax(int[] x, int[] y)
     {
         return new ByRowsMaxElemsComparer().Compare(x, y);
     }
 
+    /// <summary>
+    /// Represent comparing two one dimensional arrays.
+    /// As a key of comparing the min elements of arrays is used.  
+    /// null is less than non-null.
+    /// </summary>
+    /// <returns>x > y -> more than zero
+    ///          x < y -> less than zero
+    ///          x = y -> zero
+    /// </returns>
     public static int ByRowsMin(int[] x, int[] y)
     {
         return new ByRowsMaxElemsComparer().Compare(x, y);
