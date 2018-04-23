d9097670bda10920addfe7bda230a39f0a6c0c3a
djbragas <windows888xxx@gmail.com>
2018-04-23 19:32:57 +0300

    Day 18 Task 6

--- NET.S.2018.Brahinets.18.Test/Task6.Tests/CustomEnumerableTests.cs
+++ NET.S.2018.Brahinets.18.Test/Task6.Tests/CustomEnumerableTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Task6.Solution;
 
 namespace Task6.Tests
 {
@@ -16,7 +17,9 @@ namespace Task6.Tests
         {
             int[] expected = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
 
-            Assert.Inconclusive();
+            int[] actual = SequenceGenerator.GetSequence(1, 1, 10, (xn, xn_1) => xn + xn_1).ToArray();
+
+            CollectionAssert.AreEqual(expected,actual);
         }
 
         [Test]
@@ -24,7 +27,9 @@ namespace Task6.Tests
         {
             int[] expected = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
-            Assert.Inconclusive();
+            int[] actual = SequenceGenerator.GetSequence(1, 2, 10, (xn, xn_1) => 2*xn).ToArray();
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [Test]
@@ -32,7 +37,22 @@ namespace Task6.Tests
         {
             double[] expected = { 1, 2, 2.5, 3.3, 4.05757575757576, 4.87086926018965, 5.70389834408211, 6.55785277425587, 7.42763417076325, 8.31053343902137 };
 
-            Assert.Inconclusive();
+            double[] actual = SequenceGenerator.GetSequence(1.0, 2.0, 10, (xn, xn_1) => xn + xn_1 / xn).ToArray();
+
+            double accuracy = 1e-7;
+
+            CollectionAssert.AreEqual(expected, actual, Comparer<double>.Create(
+                (a, b) =>
+                {
+                    if (Math.Abs(a - b) <= accuracy)
+                    {
+                        return 0;
+                    }
+
+                    return (int)(a - b);
+                }));
         }
+
+
     }
 }
---
+++ NET.S.2018.Brahinets.18.Test/Test6.Solution/SequenceGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6.Solution
+{
+    public static class SequenceGenerator
+    {
+        public static IEnumerable<T> GetSequence<T>(T x1, T x2, int n, Func<T, T, T> recurrentRelation)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(n)} must be more or equal to 0");
+            }
+
+            if (n == 0)
+            {
+                return Enumerable.Empty<T>();
+            }
+
+            if(recurrentRelation == null)
+            {
+                throw new ArgumentNullException($"{nameof(recurrentRelation)} is null");
+            }
+
+            return ConstructSequence(x1, x2, n, recurrentRelation);
+        }
+
+        private static IEnumerable<T> ConstructSequence<T>(T x1, T x2, int n, Func<T, T, T> recurrentRelation)
+        {
+           
+            yield return x1;
+
+            if (n == 1)
+            {
+                yield break;
+            }
+
+            yield return x2;
+
+            T curr = default(T);
+            T xn = x2;
+            T xn_1 = x1;
+
+            for(int i = 3; i <= n; i++)
+            {
+                curr = recurrentRelation(xn, xn_1);
+                yield return curr;
+                xn_1 = xn;
+                xn = curr;
+            }
+
+            yield break;
+        }
+
+    }
+}
